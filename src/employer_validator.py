de6b264c1b7ad71eae4745ea56577cbba6c8adb6
rybin <rybin.32@gmail.com>
2025-10-26 22:06:20 +0300

    инициализация класса для валидации данных работодателей

--- src/employer_validator.py
+++ src/employer_validator.py
@@ -10,4 +10,38 @@ class Employer(EmployerAbstract):
     """
     Класс для валидации данных работодателей
     """
-    pass
+    __slots__ = ('id', 'name', 'site_url')
+
+    __id: int
+    __name: str
+    __site_url: str
+
+    def __init__(self, id, name, site_url):
+        super.__init__()
+        self.__id = id
+        self.__name = name
+        self.__site_url = site_url
+
+    @classmethod
+    def from_dict(cls, dict_employers):
+        """
+        Метод преобразования словаря в экземпляр класса
+        """
+        return cls(
+            id=dict_employers.get("name", ""),
+            name=dict_employers.get("name", ""),
+            site_url=dict_employers.get("site_url"),
+            ),
+
+    def to_dict(self):
+        """
+        Метод преобразования экземпляра класса в словарь
+        """
+        return {
+            "id": self.__id,
+            "name": self.__name,
+            "site_url": self.__site_url,
+        }
+
+    def __str__(self):
+        return f"id компании: {self.__id}\nназвание компании: {self.__name}\nссылка на сайт: {self.__site_url}"
